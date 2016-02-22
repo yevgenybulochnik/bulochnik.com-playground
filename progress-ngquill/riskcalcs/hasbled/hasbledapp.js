0da8199ba5aa3affa08c05c6997fa767a4ad87de
Yevjen <Yevgeny.bulochnik@gmail.com>
2016-02-21 16:17:18 -0800

    Added hasbled controller.

--- progress-ngquill/riskcalcs/hasbled/hasbledapp.js
+++ progress-ngquill/riskcalcs/hasbled/hasbledapp.js
@@ -1,1 +1,27 @@
-test
+var app = angular.module("hasbled", []);
+
+app.controller("hasbledctrl",function(){
+  this.hasbled =  {
+  riskcalc_name:"HasBled",
+  riskfactors:[
+    {name:"HTN", value: 1, isclicked: false},
+    {name:"Renal", value: 1, isclicked: false},
+    {name:"Liver", value: 1, isclicked: false},
+    {name:"CVA", value: 1, isclicked: false},
+    {name:"Bleed", value: 1, isclicked: false},
+    {name:"Labile INR", value: 1, isclicked: false},
+    {name:"Age >65", value: 1, isclicked: false},
+    {name:"ETOH", value: 1, isclicked: false},
+    {name:"Thinners", value: 1, isclicked: false}
+  ],
+  scores:[
+    "",
+    "3.4%",
+    "4.1%",
+    "5.8%",
+    "8.9%",
+    "9.1%",
+    ">10%"
+  ]
+};
+});
