298307215de099212d7fada6d13694014fa8ddcb
Yevjen <Yevgeny.bulochnik@gmail.com>
2016-02-25 22:00:26 -0800

    Gen assessments for both risk calcs

--- progress-ngquill/riskcalcs/hasbled/hasbled.js
+++ progress-ngquill/riskcalcs/hasbled/hasbled.js
@@ -1,25 +1,48 @@
 var app = angular.module("hasbled", []);
 
 app.controller("hasbledctrl",function(){
+  this.score = 0;
+  this.percent = "";
+  this.clicked_factors = [];
   this.clicked = function(factor){
     if(factor.isclicked){
       factor.isclicked= false;
+      this.score -= factor.value;
+      var index = this.clicked_factors.indexOf(factor.abv);
+      this.clicked_factors.splice(index,1);
     }else{
       factor.isclicked = true;
+      this.score += factor.value;
+      this.clicked_factors.push(factor.abv);
+    }
+  this.percent = this.hasbled.scores[this.score];
+  this.getassessment(this.hasbled.riskcalc_name,this.score, this.percent, this.clicked_factors);
+  };
+  this.getassessment = function(riskcalc_name, score,percent, clicked_factors){
+    var text = 'Patient has a '+riskcalc_name+'='+score+' (';
+    if(clicked_factors.length === 0){
+      text = "";
+    }else if(clicked_factors.length == 1){
+      text+=clicked_factors[0]+") corresponding to a "+percent+" annual bleed risk";
+    }else{
+      for(i=0;i<clicked_factors.length-1;i++){
+        text+=clicked_factors[i]+', ';
+      }
+      text+=clicked_factors[clicked_factors.length-1]+') corresponding to a '+percent+" annual bleed risk";
     }
   };
   this.hasbled =  {
   riskcalc_name:"HasBled",
   riskfactors:[
-    {name:"HTN", value: 1, isclicked: false},
-    {name:"Renal", value: 1, isclicked: false},
-    {name:"Liver", value: 1, isclicked: false},
-    {name:"CVA", value: 1, isclicked: false},
-    {name:"Bleed", value: 1, isclicked: false},
-    {name:"Labile INR", value: 1, isclicked: false},
-    {name:"Age >65", value: 1, isclicked: false},
-    {name:"ETOH", value: 1, isclicked: false},
-    {name:"Thinners", value: 1, isclicked: false}
+    {name: "Hypertension", abv:"HTN", value: 1, isclicked: false},
+    {name: "Renal Failure", abv:"Renal", value: 1, isclicked: false},
+    {name: "Liver Failure", abv:"Liver", value: 1, isclicked: false},
+    {name: "CVA/TIA", abv:"CVA", value: 1, isclicked: false},
+    {name: "Bleeding History", abv:"Bleed", value: 1, isclicked: false},
+    {name: "Labile INR", abv:"Labile INR", value: 1, isclicked: false},
+    {name: "Age > 65", abv:"Age >65", value: 1, isclicked: false},
+    {name: "ETOH Intake", abv:"ETOH", value: 1, isclicked: false},
+    {name: "Other anticoagulants", abv:"Thinners", value: 1, isclicked: false}
   ],
   scores:[
     "",
@@ -28,6 +51,9 @@ app.controller("hasbledctrl",function(){
     "5.8%",
     "8.9%",
     "9.1%",
+    ">10%",
+    ">10%",
+    ">10%",
     ">10%"
   ]
 };
@@ -41,7 +67,7 @@ app.directive("hasbled",function(){
     controllerAs:'ctrl',
     template:`
   <button ng-click='ctrl.clicked(factor)' ng-class='{active:factor.isclicked}' ng-repeat='factor in ctrl.hasbled.riskfactors'>
-    {{factor.name}}
+    {{factor.abv}}
     <div class='right'>+</div>
   </button>`
 };
