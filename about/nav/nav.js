e13c8cde3bd0542e70c72bbb84a0e85d980af844
Yevjen <Yevgeny.bulochnik@gmail.com>
2016-03-10 22:41:54 -0800

    Adjusted nav templateUrl added model to controller

--- about/nav/nav.js
+++ about/nav/nav.js
@@ -2,7 +2,22 @@ var app = angular.module("NavMenu",[]);
 
 
 app.controller("navctrl",function(){
-
+  this.links =  [
+      {link: "About", visible: true, sublink:[]},
+      {link: "Coding Projects", visible: true,
+       sublink:{links:[
+         {link:"Progress Note Editor",visible: true},
+         {link:"Electronic CV",visible: true}
+       ]}
+      } ,
+      {link: "Climbing", visible: true, sublink: []},
+      {link: "Contact", visiible: true,
+      sublink:{links:[
+        {link: "Github", visible: true},
+        {link: "Email", visible: true}
+       ]}
+      }
+     ];
 });
 
 app.directive('euNav',function(){
@@ -10,15 +25,7 @@ app.directive('euNav',function(){
     scope: {},
     restrict: "E",
     controller: "navctrl",
+    controllerAs: "nav",
     templateUrl: "nav/nav.html",
-    link: function(){
-      $('nav').mouseenter(function(){
-          $('li li').stop().show(500);
-      });
-
-      $('nav').mouseleave(function(){
-          $('li li').stop().hide(500);
-      });
-    }
   };
 });
