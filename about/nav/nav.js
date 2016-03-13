5c34bab50f21c231a6c6ae13dc938e23f903d9e4
Yevjen <Yevgeny.bulochnik@gmail.com>
2016-03-13 09:48:29 -0700

    Added angular mouseover effect

--- about/nav/nav.js
+++ about/nav/nav.js
@@ -3,21 +3,39 @@ var app = angular.module("NavMenu",[]);
 
 app.controller("navctrl",function(){
   this.links =  [
-      {link: "About", visible: true, sublink:[]},
-      {link: "Coding Projects", visible: true,
-       sublink:{links:[
-         {link:"Progress Note Editor",visible: true},
-         {link:"Electronic CV",visible: true}
-       ]}
+      {link: "About", sublink:[]},
+      {link: "Coding Projects",
+       sublink:[
+         {link:"Progress Note Editor",visible: false},
+         {link:"Electronic CV",visible: false}
+       ]
       } ,
-      {link: "Climbing", visible: true, sublink: []},
-      {link: "Contact", visiible: true,
-      sublink:{links:[
-        {link: "Github", visible: true},
-        {link: "Email", visible: true}
-       ]}
+      {link: "Climbing", sublink: []},
+      {link: "Contact",
+      sublink:[
+        {link: "Github", visible: false},
+        {link: "Email", visible: false}
+       ]
       }
      ];
+     this.nav_mouseenter = function(){
+       for(i=0;i<this.links.length;i++){
+        if(this.links[i].sublink.length){
+          for(n=0;n<this.links[i].sublink.length;n++){
+            this.links[i].sublink[n].visible = true;
+          }
+        }
+       }
+     };
+    this.nav_mouseleave = function(){
+     for(i=0;i<this.links.length;i++){
+      if(this.links[i].sublink.length){
+        for(n=0;n<this.links[i].sublink.length;n++){
+          this.links[i].sublink[n].visible = false;
+        }
+      }
+     }
+   };
 });
 
 app.directive('euNav',function(){
