df4e6de93da62bb6fe8db977aa1945df82c862d9
jerrywardlow <jerrylikesbikes@gmail.com>
2016-04-24 23:13:32 -0700

    Using Forever to run server.js

--- about/vm_config.sh
+++ about/vm_config.sh
@@ -4,5 +4,11 @@ apt-get update -qqy
 apt-get install -qqy git
 apt-get install -qqy build-essential
 curl -sL https://deb.nodesource.com/setup_5.x | bash
-apt-get isntall -qqy nodejs
+apt-get install -qqy nodejs
+
 npm install -g bower
+npm install -g forever
+
+cd /bulochnik.com
+forever start server.js
+echo "Forever is now running server.js on 172.69.69.69:3000" > /etc/motd
