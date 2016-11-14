630c803ba5ee605a6d4dd456e21ada86cfc0256d
root <root@ip-172-31-25-223.us-west-2.compute.internal>
2016-11-14 16:31:06 +0000

    Inital commit, added eu-nav component

--- ng2bul/src/app/app.module.ts
+++ ng2bul/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { EunavComponent } from './eunav/eunav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EunavComponent
   ],
   imports: [
     BrowserModule,
